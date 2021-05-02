fc671d322b01eeaea666a1c400ec58cbdbc4cf89
rusan3 <rm001995@hotmail.com>
2021-05-02 16:21:12 +0900

    update rupag.py and make rupag_easy.py by RU3

--- rupag_easy.py
+++ rupag_easy.py
@@ -116,39 +116,51 @@ class Autogui:
 
 if __name__ == "__main__":
 
-    # class 実施例
-    gui0 = Autogui(interval=1)
-    gui1 = Autogui(500, 500)
-    gui2 = Autogui(500, 600)
-    gui3 = Autogui(600, 500)
-    gui4 = Autogui(600, 600)
-    gui5 = Autogui(700, 500)
-    gui6 = Autogui(700, 600)
-    gui7 = Autogui(700, 600)
-    gui8 = Autogui(
-        path='C:\Program Files\Google\Chrome\Application\chrome.exe')
-    gui9 = Autogui(interval=3)
-    gui13 = Autogui(delaytime=30, interval=3, image='chrome.png')
-    gui10 = Autogui(hk1='Altleft', hk2='f4')
-    gui11 = Autogui(700, 600)
-    gui12 = Autogui(write='qwertyuiop')
-
+    # CSV input 実施例
+    print('input arg :', sys.argv[1])
+    intable = pd.read_csv('input_table/'+sys.argv[1], index_col=0)
     start = time.time()
 
-    gui0.pbar()
-    gui1.move()
-    gui2.move()
-    gui3.move()
-    gui4.move()
-    gui5.move()
-    gui6.move()
-    gui7.click()
-    gui8.do_exe()
-    gui9.wait()
-    gui13.delay()
-    gui10.hotkey()
-    gui11.click()
-    gui12.keybord()
+    for i in intable.index:
+
+        in_func = intable.iloc[i, 0]
+        in_arg = intable.iloc[i, 1:]
+
+        if i == 0:
+            old_arg = in_arg.to_list()
+
+        # str の引数は エラー回避のため 毎回 再str化しておく
+        in_arg = [str(in_arg[k]) if (k in [4, 5, 6, 7, 8, 9, 10])
+                  == True else in_arg[k] for k in range(len(in_arg))]
+
+        # str 以外の引数で nan の場合は 前の引数を参照する
+        for j in range(len(in_arg)):
+            if (j in [4, 5, 6, 7, 8, 9, 10]) == False:
+                in_arg[j] = old_arg[j] if math.isnan(
+                    in_arg[j]) == True else in_arg[j]
+
+        old_arg = in_arg
+        if i == 0:
+            continue
+
+        gui = Autogui(*in_arg)
+
+        if in_func == 'move':
+            gui.move()
+        if in_func == 'click':
+            gui.click()
+        if in_func == 'keybord':
+            gui.keybord()
+        if in_func == 'hotkey':
+            gui.hotkey()
+        if in_func == 'do_exe':
+            gui.do_exe()
+        if in_func == 'wait':
+            gui.wait()
+        if in_func == 'delay':
+            gui.delay()
+        if in_func == 'pbar':
+            gui.pbar()
 
     print('')
     print('Total time :', round(time.time()-start, 2), 'sec')
